1ef441745d9614399ad78fedfa0ee3823865a9bf
Sam Bingham <131708684+sambingham0@users.noreply.github.com>
2025-06-06 19:06:30 -0600

    cred moved

---
+++ frontend/.angular/cache/20.0.1/frontend/vite/deps/@angular_fire_app.js
@@ -0,0 +1,63 @@
+import {
+  DEFAULT_ENTRY_NAME,
+  FirebaseApp,
+  FirebaseAppModule,
+  FirebaseApps,
+  FirebaseError,
+  SDK_VERSION,
+  _addComponent,
+  _addOrOverwriteComponent,
+  _apps,
+  _clearComponents,
+  _components,
+  _getProvider,
+  _isFirebaseApp,
+  _isFirebaseServerApp,
+  _registerComponent,
+  _removeServiceInstance,
+  _serverApps,
+  deleteApp,
+  firebaseApp$,
+  getApp2 as getApp,
+  getApps,
+  initializeApp,
+  initializeServerApp,
+  onLog,
+  provideFirebaseApp,
+  registerVersion2 as registerVersion,
+  setLogLevel
+} from "./chunk-EDPJRCRF.js";
+import "./chunk-KJ2UX65A.js";
+import "./chunk-TYLNY5KT.js";
+import "./chunk-FQBI3WRC.js";
+import "./chunk-4DJR3ZNH.js";
+import "./chunk-5K356HEJ.js";
+export {
+  FirebaseApp,
+  FirebaseAppModule,
+  FirebaseApps,
+  FirebaseError,
+  SDK_VERSION,
+  DEFAULT_ENTRY_NAME as _DEFAULT_ENTRY_NAME,
+  _addComponent,
+  _addOrOverwriteComponent,
+  _apps,
+  _clearComponents,
+  _components,
+  _getProvider,
+  _isFirebaseApp,
+  _isFirebaseServerApp,
+  _registerComponent,
+  _removeServiceInstance,
+  _serverApps,
+  deleteApp,
+  firebaseApp$,
+  getApp,
+  getApps,
+  initializeApp,
+  initializeServerApp,
+  onLog,
+  provideFirebaseApp,
+  registerVersion,
+  setLogLevel
+};
